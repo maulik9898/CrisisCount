458c42364c5630f05c7faacfad91ba6f75d1af47
Maulik Patel <maulikp163@gmail.com>
2023-06-09 14:29:39 +0000

    updated name

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
       manifest: {
-        name: "CrisisCount",
+        name: "CrisisCountAdmin",
         short_name: "crisisCount",
 
         icons: [
--- guard/vite.config.ts
+++ guard/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
       manifest: {
-        name: "CrisisCount",
+        name: "CrisisCountGuard",
         short_name: "crisisCount",
 
         icons: [
